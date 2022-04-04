13b7b1f4e47befc8e6736e6e21c14960a142b584
WaiYikWong <19122@wc.school.nz>
2022-04-04 14:21:28 +1200

    rescaled sizes and added in a spawn manager

--- Game/Assets/Scripts/SpawnManager.cs
+++ Game/Assets/Scripts/SpawnManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
+
 
     // Update is called once per frame
     void Update()
