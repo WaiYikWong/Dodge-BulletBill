1049d01d2de4429d9cc878886e19566318b10e53
WaiYikWong <19122@wc.school.nz>
2022-04-05 15:10:24 +1200

    fixed spawn manager

--- Game/Assets/Scripts/FollowPlayer.cs
+++ Game/Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 4, -6);
+    private Vector3 offset = new Vector3(0, 4, -9);
     // Start is called before the first frame update
     void Start()
     {
--- Game/Assets/Scripts/MovingLeft.cs
+++ Game/Assets/Scripts/MovingLeft.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovingLeft : MonoBehaviour
 {
-    private float speed = 500;
+    private float speed = 10;
     // Start is called before the first frame update
     void Start()
     {
